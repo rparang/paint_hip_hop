2185644668e00bfc9ba076ce589fdcafdc103b48
rparang <reza@vitrue.com>
2012-04-17 21:17:27 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jams::Application.config.secret_token = 'ff095e35341b3f12b4d75697bf9911d2f1028a8cbe512a10b7b5021d052ed6694135d518c21e17459fce5be719e8d89125893d35d540539e094cd80d60ef65ce'

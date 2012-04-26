549cd9533be8529032a7372092b9cbd34b301190
rparang <reza@vitrue.com>
2012-04-25 22:10:49 -0700

    user sessions and updates/deletes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, :only => [:edit, :update]
+  before_filter :correct_user,   :only => [:edit, :update]
+  before_filter :admin_user,     :only => :destroy
+  
   
   def show
     @user = User.find(params[:id])
@@ -19,8 +23,49 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
   def index
     @users = User.all
   end
   
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      sign_in @user
+      flash[:success] = "Successfully updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_path
+  end
+  
+  
+  private
+  
+    def signed_in_user
+      unless signed_in?
+        store_location
+        flash[:notice] = "Please sign in to do that"
+        redirect_to signin_path
+      end
+    end
+  
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+    
 end

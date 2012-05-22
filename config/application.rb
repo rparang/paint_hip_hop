0bb8093dc7e722acc770c8239c27f940bd86c0d9
rparang <reza@vitrue.com>
2012-05-21 22:24:59 -0700

    stuff

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,7 @@ module Jams
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.sass.load_paths << File.expand_path('../../lib/assets/stylesheets/')
   end
 end

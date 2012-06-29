e636293b1ac69d01a7b614c4d396cdaba096e268
rparang <reza@vitrue.com>
2012-06-28 22:19:42 -0700

    updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,6 +48,8 @@ class User < ActiveRecord::Base
   validates :password, :length => { minimum: 6 }
   validates :password_confirmation, :presence => true
 
+  default_scope order: 'users.created_at ASC'
+
   def feed
     Video.from_users_following(self)
   end

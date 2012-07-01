621fc7e1290be92ebc79d4a9a89e1820d456e1c4
rparang <reza@vitrue.com>
2012-07-01 15:12:58 -0700

    updates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   def home
     @vote = Vote.new
   	if signed_in?
-  		@feed_items = current_user.feed.paginate(:page => params[:page], :per_page => 10)
+  		@feed_items = Video.paginate(:page => params[:page], :per_page => 10)
       if request.xhr?
         render :partial => 'shared/feed'
       end

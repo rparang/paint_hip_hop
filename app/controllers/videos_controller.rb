2a6f3eb22ef8a8ba34b27626e6a7883ece12a7f5
rparang <reza@vitrue.com>
2012-09-16 17:01:42 -0700

    home page bug where user gets most recent videos but then begins seeing top videos in the same feed

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -16,6 +16,7 @@ class VideosController < ApplicationController
     #@videos = Video.paginate(:page => params[:page], :per_page => 2)
     @vote = Vote.new(params[:vote])
     @feed_items = Video.desc_videos.paginate(:page => params[:page], :per_page => 10)
+    #@feed_items = Video.paginate(:page => params[:page], :per_page => 10)
     if request.xhr?
       render :partial => 'shared/feed'
     end
